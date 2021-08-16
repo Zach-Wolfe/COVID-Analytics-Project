8166e6b0fea3e25c58082f05ca4efb7f67cec2a5
Zach <85144283+Zach-Wolfe@users.noreply.github.com>
2021-08-16 16:15:52 -0400

    Add files via upload

--- Case Totals And Health Script.sql
+++ Case Totals And Health Script.sql
@@ -39,6 +39,7 @@ SET [Vaccinations Per Date] = 0 WHERE [Vaccinations Per Date] = NULL
 --Creating Health Table--
 SELECT 
 Country,
+CAST(Population AS DECIMAL) AS 'Population',
 MAX([GDP Per Capita]) as 'GDP Per Capita',
 MAX([Extreme Poverty Pertentage]) as 'Extreme Poverty Pertentage',
 MAX([Cardiovascular Death Rate]) as 'Cardiovascular Death Rate',
@@ -46,7 +47,7 @@ MAX([Diabetes Prevalence]) as 'Diabetes Prevalence',
 MAX([Female Smokers]) as 'Percent Female Smokers',
 MAX([Male Smokers]) as 'Percent Male Smokers'
 FROM [COVID Project].dbo.[Clean-COVID-Data]
-GROUP BY Country
+GROUP BY Country, [Population]
 ORDER BY Country
 
 --Removing unnecessary rows (where country column is populated by continent and countries without data)--
@@ -60,17 +61,27 @@ WHERE [Cardiovascular Death Rate]=''
 
 
 
---Creating Up-To-Date Table For Total Cases, Vaccinations, Tests Per Country--
+--Creating Up-To-Date Table For Total Cases, Vaccinations, Tests, etc.--
 SELECT 
-Country, 
+[Country], 
 CAST(Population AS DECIMAL) AS 'Population',
 MAX(CAST([Cases Per Date] AS DECIMAL)) as 'Total Cases',
 MAX(CAST([Deaths Per Date] AS DECIMAL)) as 'Total Deaths',
 MAX(CAST([Tests Per Date] AS DECIMAL)) as 'Total Tests',
-MAX(CAST([Vaccinations Per Date] AS DECIMAL)) as 'Vaccinations Per Date'
-FROM [COVID Project].dbo.[Clean-COVID-Data]
-GROUP BY Country, Population
-ORDER BY Country
+MAX(CAST([Vaccinations Per Date] AS DECIMAL)) as 'Vaccinations Per Date',
 
+--Creating Percentage Stats--
+LEFT(MAX(CAST([Cases Per Date] AS DECIMAL)) / [Population], 6) AS 'Percent Infected',
+LEFT(MAX(CAST([Vaccinations Per Date] AS DECIMAL)) / [Population], 6) AS 'Percent Vaccinated',
+LEFT(MAX(CAST([Tests Per Date] AS DECIMAL)) / [Population], 6) AS 'Percentage Tested',
+LEFT(MAX(CAST([Deaths Per Date] AS DECIMAL)) / MAX(CAST([Cases Per Date] AS DECIMAL)), 6) AS 'Death Rate',
+LEFT(MAX(CAST([Tests Per Date] AS DECIMAL)) / MAX(CAST([Cases Per Date] AS DECIMAL)), 6) AS ' Infected Testing Rate',
+LEFT(MAX(CAST([Vaccinations Per Date] AS DECIMAL)) / MAX(CAST([Cases Per Date] AS DECIMAL)), 6) AS 'Vaccination Rate'
+
+FROM [COVID Project].dbo.[Clean-COVID-Data]
+GROUP BY [Country], Population
+ORDER BY [Country]
+
+--Removing unnecessary rows--
 DELETE FROM [COVID Project].dbo.[Clean-COVID-Data]
-WHERE [Cases Per Date]='' OR [Deaths Per Date]='' OR [Tests Per Date]='' OR [Vaccinations Per Date]=''
+WHERE [Cases Per Date]='' OR [Deaths Per Date]='' OR [Tests Per Date]='' OR [Vaccinations Per Date]='' OR [Cases Per Date]=0
